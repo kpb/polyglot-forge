397e3ef5c3ce2abcb3408b1a8e1606bc9ba45b32
Kenneth Bowen <kenneth@kennethbowen.com>
2026-01-02 09:55:22 -0700

    chore(python): ruff format python files

--- blueprint/python/tests/conftest.py
+++ blueprint/python/tests/conftest.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import subprocess
-from collections.abc import Iterator
 
 import pytest
 
--- blueprint/python/tests/integration/test_redis_container.py
+++ blueprint/python/tests/integration/test_redis_container.py
@@ -4,4 +4,4 @@ import redis
 
 @pytest.mark.integration
 def test_redis_container_ping(redis_client: redis.Redis):
-        assert redis_client.ping()
+    assert redis_client.ping()
